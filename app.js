6a2b11c6b0e32098facaf9fe7e4d99771f3e8632
Amit952397 <sulovewithparents@gmail.com>
2025-09-19 23:56:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h2 className="title">Products List</h2>
+      <div className="product-list">
+        <ProductCard name="Wireless Mouse" price="25.99" status="In Stock" />
+        <ProductCard name="Keyboard" price="45.50" status="Out of Stock" />
+        <ProductCard name="Monitor" price="199.99" status="In Stock" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
